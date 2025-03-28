7478f8e207f03f7cd43841fbace4585d7ef2d6cd
AliciaMartinezA <alicia.martinezENG@outlook.es>
2025-03-28 12:38:16 +0100

    ModuloMegaMarket[UP] : modelo

--- mega_market/models/models.py
+++ mega_market/models/models.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class MegaMarketElectrodomestico(models.Model):
+    _name = 'mega_market.electrodomestico'
+    _description = 'Electrodoméstico'
 
-# class mega_market(models.Model):
-#     _name = 'mega_market.mega_market'
-#     _description = 'mega_market.mega_market'
+    name = fields.Char('Nombre')
+    codigo_producto = fields.Char('Código de Producto')
+    pais_id = fields.Many2one('res.country', 'País', required=True)
+    importe_compra = fields.Float('Importe de Compra')
+    moneda_id = fields.Many2one('res.currency', 'Moneda de Compra', required=True)
+    precio_venta = fields.Float('Precio de Venta')
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class MegaMarketCliente(models.Model):
+    _name = 'mega_market.cliente'
+    _description = 'Cliente'
+
+    name = fields.Char('Nombre')
+    apellidos = fields.Char('Apellidos')
+    nif = fields.Char('NIF')
+    direccion = fields.Text('Dirección')
+    fecha_nacimiento = fields.Date('Fecha de Nacimiento')
+    telefono = fields.Char('Teléfono')
 
