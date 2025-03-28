83630b9adc49949cc7ab197fdb138af3e6be6266
AliciaMartinezA <alicia.martinezENG@outlook.es>
2025-03-28 13:17:53 +0100

    ModuloZoo [UP] : modelos

--- zoo/controllers/controllers.py
+++ zoo/controllers/controllers.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Zoo(http.Controller):
-#     @http.route('/zoo/zoo', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Zoo(http.Controller):
+     @http.route('/zoo/zoo', auth='public')
+     def index(self, **kw):
+         return "Hello, world"
 
-#     @http.route('/zoo/zoo/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('zoo.listing', {
-#             'root': '/zoo/zoo',
-#             'objects': http.request.env['zoo.zoo'].search([]),
-#         })
+     @http.route('/zoo/zoo/objects', auth='public')
+     def list(self, **kw):
+         return http.request.render('zoo.listing', {
+             'root': '/zoo/zoo',
+             'objects': http.request.env['zoo.zoo'].search([]),
+         })
 
-#     @http.route('/zoo/zoo/objects/<model("zoo.zoo"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('zoo.object', {
-#             'object': obj
-#         })
+     @http.route('/zoo/zoo/objects/<model("zoo.zoo"):obj>', auth='public')
+     def object(self, obj, **kw):
+         return http.request.render('zoo.object', {
+             'object': obj
+         })
 
--- zoo/models/models.py
+++ zoo/models/models.py
@@ -1,19 +1,51 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class zoo(models.Model):
-#     _name = 'zoo.zoo'
-#     _description = 'zoo.zoo'
+from odoo import models, fields
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class Animal(models.Model):
+    _name = 'zoo.animal'
+    _description = 'Animal del Zoológico'
 
+    name = fields.Char(string='Nombre')
+    species = fields.Char(string='Especie')
+    identifier = fields.Char(string='Identificador')
+    country_id = fields.Many2one('res.country', string='País de Procedencia', required=True)
+    age = fields.Integer(string='Edad')
+    entry_date = fields.Date(string='Fecha de Entrada')
+
+
+class Cuidador(models.Model):
+    _name = 'zoo.cuidador'
+    _description = 'Cuidador del Zoológico'
+
+    name = fields.Char(string='Nombre')
+    surname = fields.Char(string='Apellidos')
+    position = fields.Selection([
+        ('supervisor', 'Supervisor'),
+        ('encargado', 'Encargado'),
+        ('cuidador', 'Cuidador')
+    ], string='Cargo')
+    hire_date = fields.Date(string='Fecha de Incorporación')
+
+
+class Espacio(models.Model):
+    _name = 'zoo.espacio'
+    _description = 'Espacio del Zoológico'
+
+    code = fields.Char(string='Código de Espacio')
+    space_type = fields.Selection([
+        ('jaula', 'Jaula'),
+        ('vallado', 'Vallado'),
+        ('acuario', 'Acuario'),
+        ('terrarium', 'Terrarium')
+    ], string='Tipo')
+    thematic_area = fields.Selection([
+        ('europa', 'Europa'),
+        ('asia', 'Asia'),
+        ('africa', 'África'),
+        ('america', 'América'),
+        ('oceania', 'Oceanía')
+    ], string='Área Temática')
